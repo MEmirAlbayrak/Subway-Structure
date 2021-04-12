ec54ed9df11e607de11ec74fb6be3af0bf150791
Bora Tunç Kaya <bora.khazix123@gmail.com>
2021-04-12 16:36:09 +0300

    LinkedList Implementation

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DLinkedList dlist = new DLinkedList();
+		
+		dlist.append("ÜSKÜDAR");
+		dlist.push("KADIKÖY");
+		dlist.push("ATAÞEHÝR");
+		dlist.push("BEÞÝKTAÞ ");
+		dlist.push("ARNAVUTKÖY");
+		dlist.printlist(dlist.head);
+		
+		
+
+	}
+
+}
---
+++ Node.java
@@ -0,0 +1,15 @@
+
+
+public class Node {
+
+	  String data;
+      Node prev;
+      Node next;
+
+      // Constructor to create a new node
+      // next and prev is by default initialized as null
+      Node(String d) { 
+    	  
+   data = d;
+    	            }
+}
