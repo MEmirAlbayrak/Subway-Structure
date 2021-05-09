7780a80c7b50c564b927f9e4533b4a8d7ca0163d
Bora Tunç Kaya <bora.khazix123@gmail.com>
2021-05-09 16:03:48 +0300

    Graph

---
+++ Graph.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Graph {
+
+	  // A utility function to add an edge in an
+    // undirected graph
+    public static void addEdge(ArrayList<ArrayList<Integer> > adj,
+                        int u, int v)
+    {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+ 
+    // A utility function to print the adjacency list
+    // representation of graph
+    public static void printGraph(ArrayList<ArrayList<Integer> > adj)
+    {
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.println("\nAdjacency list of vertex" + i);
+            System.out.print("head");
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(" -> "+adj.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+ 
+}
--- Main.java
+++ Main.java
@@ -1,16 +1,49 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		DLinkedList dlist = new DLinkedList();
+		DLinkedList M1 = new DLinkedList();
 		
+		DLinkedList M2 = new DLinkedList();
+		
+		
+		
+		M1.append("KÝRAZLI");
+		M1.push("BAÐCILAR");
+		M1.push("ATAÞEHÝR");
+		M1.push("BEÞÝKTAÞ ");
+		M1.push("ARNAVUTKÖY");
+		M1.printlist(M1.head);
+		
+	       int V = 5;
+	        ArrayList<ArrayList<Integer> > adj
+	                    = new ArrayList<ArrayList<Integer> >(V);
+	         
+	        for (int i = 0; i < V; i++)
+	            adj.add(new ArrayList<Integer>());
+	 
+	        // Adding edges one by one
+	        Graph.addEdge(adj, 0, 1);
+	        Graph.addEdge(adj, 0, 4);
+	        Graph.addEdge(adj, 1, 2);
+	        Graph.addEdge(adj, 1, 3);
+	        Graph.addEdge(adj, 1, 4);
+	        Graph.addEdge(adj, 2, 3);
+	        Graph.addEdge(adj, 3, 4);
+	         
+	        Graph.printGraph(adj);
+		
+<<<<<<< Updated upstream
 		dlist.append("USKUDAR");
 		dlist.push("KADIKIY");
 		dlist.push("ATASEHIR");
 		dlist.push("BESIKTAS ");
 		dlist.push("ARNAVUTKOY");
 		dlist.printlist(dlist.head);
+=======
+>>>>>>> Stashed changes
 		
 		
 
